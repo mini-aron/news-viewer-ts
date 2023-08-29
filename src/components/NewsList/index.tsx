78e9c4a2e75eac071e52f93780adeb183eb6c4b2
우엥 <105274015+mini-aron@users.noreply.github.com>
2023-08-29 23:49:47 +0900

    add: add react-query

--- src/components/NewsList/index.tsx
+++ src/components/NewsList/index.tsx
@@ -3,32 +3,43 @@ import { articleType } from "../../types/components/articleType";
 import { NewsListPropsTypes } from "../../types/components/NewsList";
 import axios from "axios";
 import * as S from "./style";
-import * as C from '../../components';
+import * as C from "../../components";
 import { usePromise } from "../../hooks";
 import EnvConfig from "../../api/EnvConfig";
+import { useQuery } from "react-query";
 
-function NewsList({ category }:NewsListPropsTypes) {
-  const [loading, response, error] = usePromise(() => {
-    const query = category === 'all' ? '' : `&category=${category}`;
-    return axios.get(
-      `${EnvConfig.NEWSAPI_URL}?country=kr${query}&apiKey=${EnvConfig.API_KEY}`,
-    );
-  },[category])
-
-  if (loading) {
+function NewsList({ category }: NewsListPropsTypes) {
+  const { isLoading, isError, data, error } = useQuery(
+    "newsList",
+    () => {
+      const query = category === "all" ? "" : `&category=${category}`;
+      return axios.get(
+        `${EnvConfig.NEWSAPI_URL}?country=kr${query}&apiKey=${EnvConfig.API_KEY}`
+      );
+    },
+    {
+      refetchOnWindowFocus: false,
+      retry: 2,
+      onSuccess: (data) => {
+        console.log(data);
+      },
+      onError: (e) => {},
+    }
+  );
+  if (isLoading) {
     return <S.NewsListBlock>대기 중...</S.NewsListBlock>;
   }
-  if (!response) {
+  if (!data) {
     return null;
   }
-  if (error) {
+  if (isError) {
     return <S.NewsListBlock>에러 발생!</S.NewsListBlock>;
   }
- 
-  const { articles } = response.data;
+
+  const { articles } = data.data;
   return (
     <S.NewsListBlock>
-      {articles.map((article:articleType) => (
+      {articles.map((article: articleType) => (
         <C.NewsItem key={article.url} article={article} />
       ))}
     </S.NewsListBlock>
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <QueryClientProvider client={queryClient}>
   <BrowserRouter>
     <App />
   </BrowserRouter>,
+  </QueryClientProvider>
 );
 
